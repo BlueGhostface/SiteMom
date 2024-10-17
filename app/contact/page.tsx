45b954f2d7d9cd5055d06099082c1c626aa8dcae
axell.tielemans <axell.tielemans@student.kdg.be>
2024-10-17 16:08:48 +0200

    header functionality updated buttons and title get disabled when at respective destinations

---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function contact() {
+    return (
+      <div>
+        <h1>page 2</h1>
+      </div>
+    );
+  }
--- app/domain/home/Header.tsx
+++ app/domain/home/Header.tsx
@@ -1,12 +1,21 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export function Header() {
   const router = useRouter();
+  const pathname = usePathname();
 
   const NavigateHome = () => {
-    router.push("/");
+    if(pathname !== '/')  {
+      router.push("/");
+    }
+  };
+
+  const NavigateContact = () => {
+    if(pathname !== '/contact') {
+      router.push("/contact");
+    }
   };
 
   return (
@@ -14,19 +23,27 @@ export function Header() {
       <div className="container mx-auto flex grow items-center justify-between">
         {/* Left Section - Title (30%) */}
         <div className="w-3/10 grow">
-          <h1 className="text-2xl font-bold" onClick={NavigateHome}>Pedicure Tine</h1>
+          <h1 className={`text-2xl font-bold ${pathname === '/' ? '' : 'cursor-pointer'}`} onClick={NavigateHome}>
+            Pedicure Tine
+          </h1>
         </div>
-
+ 
         {/* Right Section - Buttons (60%) */}
         <div className="w-6/10 flex grow justify-center space-x-4">
-          <Button className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Home
-          </Button>
-          <Button className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          <Button
+            disabled={pathname === "/contact"}
+            className={`${pathname === "/contact" ? " bg-red-500" : ""}`}
+            onClick={NavigateContact}
+          >
             Contact
           </Button>
-          <Button className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-            FAQ
+          <Button
+            disabled={pathname === "/"}
+            className={`${pathname === "/" ? " bg-red-500" : ""} `}
+            onClick={NavigateHome}
+          >
+            {" "}
+            Home
           </Button>
         </div>
       </div>
