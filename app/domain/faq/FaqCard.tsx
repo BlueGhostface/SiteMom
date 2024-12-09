9e1674b0df27aa1adcd5a86debd66c0d6e050c7f
axell.tielemans <axelltielemans@gmail.com>
2024-12-09 18:44:09 +0100

    update faqcard 2de question

--- app/domain/faq/FaqCard.tsx
+++ app/domain/faq/FaqCard.tsx
@@ -5,10 +5,24 @@ export default function FaqCard() {
         Hoe maak ik een afspraak ?
       </div>
       <div className="text-justify">
-        Wenst u een afspraakt te maken dan moet u telefonisch contact met mij opnemen. 
-        In het geval dat ik op dat bepaald moment niet aan de lijn kan komen gelieven een bericht in te spreken 
-        met uw naam, telefoon nummer, adres en de eventuele klachten die u waarneemt. 
-        Hierna probeer ik zo snel mogelijk u terug te contacteren. </div>
+        Wenst u een afspraakt te maken dan moet u telefonisch contact met mij
+        opnemen. In het geval dat ik op dat bepaald moment niet aan de lijn kan
+        komen gelieven een bericht in te spreken met uw naam, telefoon nummer,
+        adres en de eventuele klachten die u waarneemt. Hierna probeer ik zo
+        snel mogelijk u terug te contacteren.
+      </div>
+
+      <div className="text-start text-xl font-bold text-Text-heading">
+        Voor wie is een behandeling bedoelt ?
+      </div>
+      <div className="text-justify">
+        Iedereen is welkom, we helpen graag de volgende personen.
+        - het hele gezin van jong tot oud 
+        - oudere personen die zich graag laten verzorgen
+        - hulpbehovende mensen die beperkt mobiel zijn of over geen vervoersmoelijkheide beschikken
+        - mensen die overdag druk bezet zijn en geen tijd hebben.
+      
+      </div>
     </div>
   );
 }
