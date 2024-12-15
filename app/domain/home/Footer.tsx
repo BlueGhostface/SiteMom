2c0c0e5eb577d22f1988810cf5c0dc0a21b13f4e
axell.tielemans <axelltielemans@gmail.com>
2024-12-15 20:16:36 +0100

    update footer nummer & creator

--- app/domain/home/Footer.tsx
+++ app/domain/home/Footer.tsx
@@ -1,7 +1,14 @@
 export default function footer() {
   return (
-    <div className="w-full grow min-h-0 bg-white ">
-      <div>hey</div>
+    <div className="w-full grow min-h-0 bg-white grid grid-cols-7 ">
+      <div className="flex-col col-start-2 ">
+        <div className="text-xl font-bold">Contact</div>
+        <div>GSM: (+32) 0468 12 78 75</div>
+      </div>
+      <div className="flex-col col-start-3">
+        <div className="text-xl font-bold">Website</div>
+        <div>LinkedIn: Axell Tielemans</div>
+      </div>
     </div>
   );
 }
