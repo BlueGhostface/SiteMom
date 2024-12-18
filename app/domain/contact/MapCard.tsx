4508d69e83dc80986b30e9201207e15500bcccbe
axell.tielemans <axelltielemans@gmail.com>
2024-12-18 19:12:36 +0100

    iframe update

--- app/domain/contact/MapCard.tsx
+++ app/domain/contact/MapCard.tsx
@@ -1,16 +1,19 @@
+"use client";
+
+const mapsrc =
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2506.1179989802276!2d4.513703200000001!3d51.087829799999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3e4cc2ff45cf3%3A0xd080940f468c20c5!2sBerthoutstraat%2014%2C%202570%20Duffel!5e0!3m2!1snl!2sbe!4v1731701788843!5m2!1snl!2sbe";
+
 export default function Mapcard() {
   return (
-    <div className="col-span-1 sm:col-span-5 sm:col-start-2 sm:m-0 rounded-lg bg-background-contact gap-3 p-2">
-      
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2506.1179989802276!2d4.513703200000001!3d51.087829799999994!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c3e4cc2ff45cf3%3A0xd080940f468c20c5!2sBerthoutstraat%2014%2C%202570%20Duffel!5e0!3m2!1snl!2sbe!4v1731701788843!5m2!1snl!2sbe"
-          width="100%"
-          height="100%"
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-          className="m-0"
-        ></iframe>
-      
+    <div className=" col-span-1 sm:col-span-5 sm:col-start-2 sm:m-0 rounded-lg bg-background-contact gap-3 p-2">
+      <iframe
+        src={mapsrc}
+        width="100%"
+        height="100%"
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+        className="m-0"
+      ></iframe>
     </div>
   );
 }
