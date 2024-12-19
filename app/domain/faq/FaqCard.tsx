e889de7cce8a0cb29104ba59134788efe9836105
axell.tielemans <axelltielemans@gmail.com>
2024-12-19 17:06:53 +0100

    update faq

--- app/domain/faq/FaqCard.tsx
+++ app/domain/faq/FaqCard.tsx
@@ -16,11 +16,10 @@ export default function FaqCard() {
         Voor wie is een behandeling bedoelt ?
       </div>
       <div className="text-justify">
-        Iedereen is welkom, we helpen graag de volgende personen.
-        - het hele gezin van jong tot oud 
-        - oudere personen die zich graag laten verzorgen
-        - hulpbehovende mensen die beperkt mobiel zijn of over geen vervoersmoelijkheide beschikken
-        - mensen die overdag druk bezet zijn en geen tijd hebben.
+        Iedereen is welkom, we helpen graag de volgende personen.<br/>
+        - Het hele gezin van jong tot oud <br/>
+        - Oudere personen die zich graag laten verzorgen<br/>
+        - Mensen die beperkt mobiel zijn of over geen vervoers mogelijkheden beschikken<br/>
       
       </div>
 
