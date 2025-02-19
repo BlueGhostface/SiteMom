000b862ebe91bd16f5d70afa648777206a09a0cc
axell.tielemans <axelltielemans@gmail.com>
2025-02-19 22:57:08 +0100

    pricebox version 1

--- app/behandeling/page.tsx
+++ app/behandeling/page.tsx
@@ -1,7 +1,8 @@
+import PriceBox from '../domain/behandeling/PriceBox';
 export default function Behandeling(){  
     return (
-          <div >
-            behandeling
+      <div className={`grid justify-between grid-cols-1 m-2 sm:grid-cols-7 gap-3 h-4/6`}>
+            <PriceBox/>
           </div>
         );
 }
--- app/domain/behandeling/PriceBox.tsx
+++ app/domain/behandeling/PriceBox.tsx
@@ -1,7 +1,54 @@
-export default function PriceBox(){
-    return (
-        <div>
-            <title>prices per person</title>
-        </div>
-    );
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const testdata = [
+  {
+    id: 1,
+    behandeling: "pedicure",
+    duurtijd: "5,",
+    inclusive: true,
+  },
+];
+
+export default function PriceBox() {
+  return (
+    <div className="row-start-2 col-span-3 mb-2 sm:m-0 sm:row-start-1 sm:col-start-3 p-2 bg-background-contact rounded-lg" >
+      <title>prices per person</title>
+
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-1/3">Type Behandeling</TableHead>
+            <TableHead className="w-1/3">Ca. duur</TableHead>
+            <TableHead className="w-1/3">Inclusive *</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {testdata.map((data) => (
+            <TableRow key={data.id}>
+              <TableCell className="w-1/3">{data.behandeling}</TableCell>
+              <TableCell className="w-1/3">{data.duurtijd} minuten</TableCell>
+              <TableCell className="w-1/3">{data.inclusive}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
+            <TableRow>
+                <TableCell colSpan={3} className="text-right">
+                    * deze behandeling zit mee ingegrepen in de standaard behandelingspakket
+                </TableCell>
+            </TableRow>
+        </TableFooter>
+      </Table>
+
+    
+    </div>
+  );
 }
